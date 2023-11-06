6dab572e419b061caae77e28e77fbac5c8b85301
Clifton Patterson <Cliftonp15@yahoo.com>
2023-11-06 14:55:21 -0500

    Added "fontswesome" packages

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-       <div className='wraper'> Hello World </div>
+       <div className='wrapper'> Hello World </div>
 
     </div>
   );
