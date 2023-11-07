86fd0005ba779a95c3f0c8c3df1ee521082ea224
Clifton Patterson <Cliftonp15@yahoo.com>
2023-11-07 13:07:21 -0500

    Added headline features

--- src/App.js
+++ src/App.js
@@ -3,8 +3,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-       <div className='wrapper'> Hello World </div>
+      <div className='to-do-wrapper'>
+      <div className='to-do-contents'>
 
+        <div className='to-do-header'>
+            <h1 className='to-do-title'> To Do List</h1>
+            <div className='new-task-button-section'> 
+            <button className='crete-new-task'> + </button> 
+            <p className='add-task'>Add Task </p>
+            </div>
+        </div>
+        <div className='task-section'> 
+          <form className='task-input'> hello</form>
+        </div>
+      </div>
+      </div>
     </div>
   );
 }
