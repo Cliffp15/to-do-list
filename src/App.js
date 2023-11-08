8ff845e8165f5ee91be928c5240aca32ed489b33
Cliffp15 <Cliftonp15@yahoo.com>
2023-11-07 22:05:49 -0500

    set up intial layout for todolist

--- src/App.js
+++ src/App.js
@@ -5,17 +5,16 @@ function App() {
     <div className="App">
       <div className='to-do-wrapper'>
       <div className='to-do-contents'>
-
         <div className='to-do-header'>
             <h1 className='to-do-title'> To Do List</h1>
             <div className='new-task-button-section'> 
-            <button className='crete-new-task'> + </button> 
-            <p className='add-task'>Add Task </p>
+            <button className='create-new-task-button'> + </button> 
+            <p className='new-task-text'>Create New Task </p>
             </div>
         </div>
-        <div className='task-section'> 
-          <form className='task-input'> hello</form>
-        </div>
+      <div className='task-section'> 
+        <form className='task-input'> hello</form>
+      </div>
       </div>
       </div>
     </div>
