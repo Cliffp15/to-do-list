82e61263461fbf8804496ce142f8ad781bac5337
Cliffp15 <Cliftonp15@yahoo.com>
2023-12-08 00:51:43 -0500

    fixed-added display functionality

--- src/App.js
+++ src/App.js
@@ -1,32 +1,12 @@
-import { useState } from 'react';
-import './App.css';
-import { Todowrapper } from './Components/Todowrapper';
-
-    
+import { useState } from "react";
+import "./App.css";
+import { Todowrapper } from "./Components/Todowrapper";
 
 function App() {
   return (
     <div className="App">
-    <Todowrapper/> 
+      <Todowrapper />
     </div>
-    
-    /*<div className="App">
-      <div className='to-do-wrapper'>
-      <div className='to-do-contents'>
-        <div className='to-do-header'>
-            <h1 className='to-do-title'> To Do List</h1>  
-        </div>
-      <div className='task-section'> 
-      <div className='new-task-button-section'> 
-            <Todo></Todo>
-      </div>
-        <form className='task-input-list'> hello</form>
-      </div>
-      </div>
-      </div>
-    </div>
-    */
-
   );
 }
 
